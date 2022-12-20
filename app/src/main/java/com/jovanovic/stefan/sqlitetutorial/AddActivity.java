4eef6888e585380d00276d3e3f78614e003a6f77
Ben Couch <benjaminwcouch@gmail.com>
2022-12-20 12:35:55 +1100

    version 1

---
+++ app/src/main/java/com/jovanovic/stefan/sqlitetutorial/AddActivity.java
@@ -0,0 +1,43 @@
+package com.jovanovic.stefan.sqlitetutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddActivity extends AppCompatActivity {
+
+    EditText title_input, author_input, pages_input, facebook_input, linkedin_input;
+    Button add_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        title_input = findViewById(R.id.title_input);
+        author_input = findViewById(R.id.author_input);
+        pages_input = findViewById(R.id.pages_input);
+        facebook_input = findViewById(R.id.facebook_input);
+        linkedin_input = findViewById(R.id.linkedin_input);
+        add_button = findViewById(R.id.add_button);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(AddActivity.this);
+                myDB.addBook(title_input.getText().toString().trim(),
+                        author_input.getText().toString().trim(),
+                        Integer.parseInt(pages_input.getText().toString().trim()),
+                        facebook_input.getText().toString().trim(),
+                        linkedin_input.getText().toString().trim());
+
+
+                   //  facebook_input.getText().toString().trim(),
+
+
+            }
+        });
+    }
+}
